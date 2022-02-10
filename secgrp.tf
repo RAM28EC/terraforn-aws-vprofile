addb8e95e7b09d2df7f784f336403f3ffa19c45a
Ram28 <ram.28ec@gmail.com>
2022-02-10 20:05:01 +0530

    ee

--- secgrp.tf
+++ secgrp.tf
@@ -69,7 +69,7 @@ resource "aws_security_group" "vprofile-backend-sg" {
     from_port = 3306
     protocol  = "tcp"
     to_port   = 3306
-    security_groups = [aws_security_group.vprofile-bastion-sg]
+    security_groups = [aws_security_group.vprofile-bastion-sg.id]
   }
 }
 resource "aws_security_group_rule" "sec_group_allow_itself" {
